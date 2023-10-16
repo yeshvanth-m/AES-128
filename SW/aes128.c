c54cd75174afe33e05acb939d51a3b11c45df1e2
Yeshvanth-M <yeshvanthmuniraj@gmail.com>
2023-10-16 06:54:19 +0530

    Added comments and created separate folder for SW and HW

--- SW/aes128.c
+++ SW/aes128.c
@@ -1,5 +1,28 @@
+/********************************************************************************
+* @file     aes128.c                                                            *
+* @brief    AES128 C implementation File                                        *
+* @author   Yeshvanth M  <yeshvanthmuniraj@gmail.com>                           *
+* @date     15-Sep-2023                                                         *
+*********************************************************************************
+*                                                                               *
+* This program is free software: you can redistribute it and/or modify it       *
+* under the terms of the GNU General Public License as published by the Free    *
+* Software Foundation, either version 3 of the License, or (at your option)     *
+* any later version.                                                            *
+*                                                                               *
+* This program is distributed in the hope that it will be useful, but           *
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY    *
+* or FITNESS FOR A PARTICULAR PURPOSE.                                          *
+* See the GNU General Public License for more details.                          *
+*                                                                               *
+* You should have received a copy of the GNU General Public License             *
+* along with this program. If not, see <https://www.gnu.org/licenses/>.         *
+*                                                                               *
+********************************************************************************/
+
 #include "aes128.h"
 
+/* The AES128 state matrix - will undergo operations */
 uint8_t aes128_state[16u] = 
 {
     0x4C, 0x6D, 0x73, 0x64,
@@ -8,6 +31,7 @@ uint8_t aes128_state[16u] =
     0x65, 0x70, 0x20, 0x6F
 };
 
+/* The input cipher key for encryption/decryption */
 uint8_t aes128_cipherKey[16u] = 
 {
     0x2B, 0x28, 0xAB, 0x09,
@@ -16,31 +40,38 @@ uint8_t aes128_cipherKey[16u] =
     0x16, 0xA6, 0x88, 0x3C
 };
 
+/* Function to add round key to the state */
 void aes128_add_round_key (uint8_t round, bool aes128_is_encrypt)
 {
     uint8_t *round_key;
 
     if (aes128_is_encrypt)
     {
+        /* Encryption process */
         if (round == 0)
         {
+            /* The input cipher key is used in round 0 */
             round_key = aes128_cipherKey;
         }
         else
         {
+            /* Use the generated keys for consecutive rounds */
             round--;
             round_key = aes128_round_keys[round];
         }
     }
     else
     {
+        /* Decryption process */
         round = 10 - round;
         if (round == 0)
         {
+            /* Input cipher key used in final round */
             round_key = aes128_cipherKey;
         }
         else
         {
+            /* Use the generated keys in other rounds */
             round--;
             round_key = aes128_round_keys[round];
         }
@@ -48,32 +79,39 @@ void aes128_add_round_key (uint8_t round, bool aes128_is_encrypt)
     
     for (uint8_t idx = 0; idx < 16u; idx++)
     {
+        /* Add the round key byte by byte */
         aes128_state[idx] ^= round_key[idx];
     }
     
 }
 
+/* Function to substitute bytes */
 void aes128_substitute_bytes (bool aes128_is_encrypt)
 {
     const uint8_t *sBoxMat;
     if (aes128_is_encrypt)
     {
+        /* Use SBox matrix in encryption */
         sBoxMat = aes128_sBox;
     }
     else
     {
+        /* Use inverse SBox in decryption */
         sBoxMat = aes128_sBox_Inv;
     }
     for (uint8_t idx = 0u; idx < 16u; idx++)
     {
+        /* Subsitutue byte by byte */
         aes128_state[idx] = sBoxMat[aes128_state[idx]];
     }
 }
 
+/* Function to shift rows */
 void aes128_shift_rows (bool aes128_is_encrypt)
 {
     if (aes128_is_encrypt)
     {
+        /* Row 0 is not shifted, other rows undergo circular left shift in encryption */
         for (uint8_t row = 1u; row < 4u; row++)
         {
             uint8_t shift = row;
@@ -92,6 +130,7 @@ void aes128_shift_rows (bool aes128_is_encrypt)
     }
     else
     {
+        /* Row 0 is not shifted, other rows undergo circular right shift in decryption */
         for (uint8_t row = 1u; row < 4u; row++)
         {
             uint8_t shift = row;
@@ -110,21 +149,26 @@ void aes128_shift_rows (bool aes128_is_encrypt)
     }
 }
 
+/* Function to mix columns */
 void aes128_mix_columns (bool aes128_is_encrypt)
 {
     uint8_t *column_mix_matrix;
 
     if (aes128_is_encrypt)
     {
+        /* Use mix column matrix for multiplication in encryption */
         column_mix_matrix = aes128_column_mix_matrix;
     }
     else
     {
+        /* Use inverse mix column matrix for multiplication in decryption */
         column_mix_matrix = aes128_column_mix_matrix_inv;
     }
 
+    /* Multiplication by X is done with the help of LUTs */
     for (uint8_t state_col = 0u; state_col < 4u; state_col++)
     {
+        /* Perform matrix multiplication */
         uint8_t state_column[4u];
         for (uint8_t matrix_row = 0u; matrix_row < 4u; matrix_row++)
         {
@@ -174,6 +218,7 @@ void aes128_mix_columns (bool aes128_is_encrypt)
             }
             state_column[matrix_row] = col_sum;
         }
+        /* Save the computed column in state */
         for (uint8_t state_row = 0; state_row < 4; state_row++)
         {
             aes128_state[(state_row * STATE_ROWS) + state_col] = state_column[state_row];
@@ -181,15 +226,18 @@ void aes128_mix_columns (bool aes128_is_encrypt)
     }
 }
 
+/* Function to generate keys using the key schedule */
 void aes128_key_schedule (void)
 {
     memcpy ((void *)aes128_round_keys[0u], (void *)aes128_cipherKey, sizeof(aes128_cipherKey));
     for (uint8_t round = 0; round < 10; round++)
     {
+        /* Form the Rcon matrix */
         uint8_t rcon[4u] = {aes128_rcon[round], 0u, 0u, 0u};
         uint8_t round_key_col[4u];
 
         uint8_t row = 0u, col = 3u, temp = aes128_round_keys[round][(row * KEY_ROWS) + col];
+        /* Obtain the round key column 3 and shift it by 1 */
         while (row < 3u)
         {
             round_key_col[row] = aes128_round_keys[round][((row + 1u) * KEY_ROWS) + col];
@@ -197,6 +245,7 @@ void aes128_key_schedule (void)
         }
         round_key_col[row] = temp;
 
+        /* Substitute bytes for the obtained column */
         for (row = 0u; row < 4u; row++)
         {
             round_key_col[row] = aes128_sBox[round_key_col[row]];
@@ -208,6 +257,7 @@ void aes128_key_schedule (void)
             {
                 for (row = 0u; row < 4u; row++)
                 {
+                    /* First column of generated key is XORed with Rcon matrix and 3rd column of previous key */
                     aes128_round_keys[round][(row * KEY_ROWS) + col] ^= (rcon[row] ^ round_key_col[row]);
                 }
             }
@@ -215,10 +265,12 @@ void aes128_key_schedule (void)
             {
                 for (row = 0u; row < 4u; row++)
                 {
+                    /* Other columns are obtained by XORing previous column of current key with column of previous key */
                     aes128_round_keys[round][(row * KEY_ROWS) + col] ^= aes128_round_keys[round][(row * KEY_ROWS) + col - 1u];
                 }
             }
         }
+        /* Save the generated key */
         memcpy ((void *)aes128_round_keys[round + 1], (void *)aes128_round_keys[round], sizeof(aes128_round_keys[round]));
     }
 }
@@ -228,9 +280,25 @@ int main()
     uint8_t round = 0u;
     bool encrypt = true, decrypt = !encrypt;
 
-    aes128_add_round_key (round, encrypt);
+    printf ("Initial state (Plain Text): ");
+    for (int i = 0; i < 16; i++)
+    {
+        printf ("%X, ", aes128_state[i]);
+    }
+
+    printf ("\nCipher Key (to key schedule): ");
+    for (int i = 0; i < 16; i++)
+    {
+        printf ("%X, ", aes128_cipherKey[i]);
+    }
+
+    /* Generate the keys through key schedule */
     aes128_key_schedule ();
 
+    /* Start encryption by adding round key to input plain text */
+    aes128_add_round_key (round, encrypt);
+
+    /* Sequence for initial 10 rounds */
     for (round = 1; round < 10; round++)
     {
         aes128_substitute_bytes (encrypt);
@@ -238,14 +306,22 @@ int main()
         aes128_mix_columns (encrypt);
         aes128_add_round_key (round, encrypt);
     }
-    
+    /* Final round does not mix columns */
     aes128_substitute_bytes (encrypt);
     aes128_shift_rows (encrypt);
     aes128_add_round_key (round, encrypt);
 
+    printf ("\nCipher Text (after encryption): ");
+    for (int i = 0; i < 16; i++)
+    {
+        printf ("%X, ", aes128_state[i]);
+    }
+
     round = 0u;
+    /* Add the 10th round key for decryption */
     aes128_add_round_key (round, decrypt);
 
+    /* Consecutive rounds of decryption follows this sequence */
     for (round = 1; round < 10; round++)
     {
         aes128_shift_rows (decrypt);
@@ -254,9 +330,14 @@ int main()
         aes128_mix_columns (decrypt);
     }
 
+    /* Last round of decryption follows this sequence */
     aes128_shift_rows (decrypt);
     aes128_substitute_bytes (decrypt);
     aes128_add_round_key (round, decrypt);
 
-    printf("Hello World");
+    printf ("\nPlain Text (after decryption): ");
+    for (int i = 0; i < 16; i++)
+    {
+        printf ("%X, ", aes128_state[i]);
+    }
 }
